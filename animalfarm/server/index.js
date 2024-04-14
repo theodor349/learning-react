255d147673e18639beea7eb6315c7a94e7c64041
Theodor Risager <theodor349@gmail.com>
2024-04-14 10:26:52 +0200

    Backend

---
+++ animalfarm/server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+import Chance from 'chance';
+const chance = new Chance();
+
+const animals = [...Array(250).keys()].map(id => {
+    return {
+      id,
+        type: chance.animal(),
+      name: chance.animal(),
+      age: chance.age
+    }
+})
+
+app.get('', (req, res) => {
+  const q = req.query.q?.toLowerCase() || '';
+  const results = animals.filter(animal => animal.name.includes(q))
+  res.send(results);
+})
+
+app.listen(8080, () => console.log('Server running on port 8080'));
