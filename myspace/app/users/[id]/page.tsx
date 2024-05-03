b5ead619cb1e510cd713da8b7785bb48d509dc05
Theodor Risager <theodor349@gmail.com>
2024-05-03 12:53:06 +0200

    Profile Page

---
+++ myspace/app/users/[id]/page.tsx
@@ -0,0 +1,36 @@
+import FollowButton from '@/components/FollowButton/FollowButton';
+import { prisma } from '@/lib/prisma';
+import { Metadata } from 'next';
+
+type Props = {
+  params: {
+    id: string;
+  };
+}
+
+export async function generateMetadata({ params }: Props) : Promise<Metadata> {
+  const user = await prisma.user.findUnique({ where: { id: params.id }});
+  return { title: `User profile of ${user?.name}`};
+}
+
+export default async function UserProfile({ params }: Props) {
+  const user = await prisma.user.findUnique({ where: { id: params.id }});
+  const {name, bio, image, id } = user ?? {};
+
+  return (
+    <div>
+      <h1>{name}</h1>
+
+      <img 
+        width={300}
+        src={image ?? '/mememan.webpg'}
+        alt={`${name}'s profile`}
+      />
+
+      <h3>Bio</h3>
+      <p>{bio}</p>
+
+      <FollowButton targetUserId={params.id} />
+    </div>
+  )
+}
