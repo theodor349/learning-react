71a77f114b9cae2a99e1dbe6bc2d86d116e58391
Theodor Risager <theodor349@gmail.com>
2024-05-08 00:08:39 +0200

    Rename

--- server-actions/src/app/page.tsx
+++ server-actions/src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Todo from "@/components/Todo/todo";
+import Todo from "@/components/Todo/Todo1";
 
 export default function Home() {
   return (
