1fdcce68f1f6cedf33deac8037c3c372138023d1
Theodor Risager <theodor349@gmail.com>
2024-05-13 14:19:24 +0200

    Get Todos

--- server-actions/src/components/Todo/Todo.tsx
+++ server-actions/src/components/Todo/Todo.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import PocketBase from "pocketbase"
 
-export default async function Todo() {
+type Todo = {
+  id: string;
+  name: string;
+}
+
+async function getTodos(){
   const pb = new PocketBase('http://127.0.0.1:8090');
-  const resultList = await pb.collection('posts').create({title, content});
+  const todos = await pb.collection('todos').getList<Todo>(1,50);
+  return todos?.items as Todo[];
+}
 
-
-  var todos = [{ id: 1, content: "Buy Milk", completed: false }, { id: 2, content: "Buy Bread", completed: false }];
+export default async function Todo() {
+  const todos = await getTodos();
   return (
     <>
       <h1>Todos Page</h1>
@@ -17,8 +24,8 @@ export default async function Todo() {
       </form>
 
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+        {todos?.map((todo) => (
+          <li key={todo.id}>{todo.name}</li>
         ))}
       </ul>
     </>
