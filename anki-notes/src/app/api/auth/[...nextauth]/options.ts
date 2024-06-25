10fe67a62a1bcb8616c1a747c71e7a75d773a227
Theodor Risager <theodor349@gmail.com>
2024-06-25 12:13:10 +0200

    Google Auth

--- anki-notes/src/app/api/auth/[...nextauth]/options.ts
+++ anki-notes/src/app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,6 @@
 import type { NextAuthOptions } from "next-auth";
 import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 
@@ -8,6 +9,10 @@ const prisma = new PrismaClient()
 export const options: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
     Github({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
