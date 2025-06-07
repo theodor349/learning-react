0fbd9696ad2fe45dd5b4383412a90c7712034170
theodor349 <theodor349@gmail.com>
2025-06-07 17:52:46 +0200

    Added initial layouts for Scroll and Layered components

---
+++ layouts-2/src/app/layered-layout/page.tsx
@@ -0,0 +1,42 @@
+export default function Page() {
+  return(
+    <div className={"flex items-center flex-col md:flex-row flex-wrap p-4"}>
+      <div className={"bg-red-100 w-full h-10"}></div>
+      <div className={"bg-red-200 w-full h-10"}></div>
+      <div className={"bg-red-300 w-full h-10"}></div>
+      <div className={"bg-red-400 w-full h-10"}></div>
+      <div className={"bg-red-500 w-full h-10"}></div>
+      <div className={"bg-red-600 w-full h-10"}></div>
+      <div className={"bg-green-100 w-full h-10"}></div>
+      <div className={"bg-green-200 w-full h-10"}></div>
+      <div className={"bg-green-300 w-full h-10"}></div>
+      <div className={"bg-green-400 w-full h-10"}></div>
+      <div className={"bg-green-500 w-full h-10"}></div>
+      <div className={"bg-green-600 w-full h-10"}></div>
+      <div className={"bg-blue-100 w-full h-10"}></div>
+      <div className={"bg-blue-200 w-full h-10"}></div>
+      <div className={"bg-blue-300 w-full h-10"}></div>
+      <div className={"bg-blue-400 w-full h-10"}></div>
+      <div className={"bg-blue-500 w-full h-10"}></div>
+      <div className={"bg-blue-600 w-full h-10"}></div>
+      <div className={"bg-red-100 w-full h-10"}></div>
+      <div className={"bg-red-200 w-full h-10"}></div>
+      <div className={"bg-red-300 w-full h-10"}></div>
+      <div className={"bg-red-400 w-full h-10"}></div>
+      <div className={"bg-red-500 w-full h-10"}></div>
+      <div className={"bg-red-600 w-full h-10"}></div>
+      <div className={"bg-green-100 w-full h-10"}></div>
+      <div className={"bg-green-200 w-full h-10"}></div>
+      <div className={"bg-green-300 w-full h-10"}></div>
+      <div className={"bg-green-400 w-full h-10"}></div>
+      <div className={"bg-green-500 w-full h-10"}></div>
+      <div className={"bg-green-600 w-full h-10"}></div>
+      <div className={"bg-blue-100 w-full h-10"}></div>
+      <div className={"bg-blue-200 w-full h-10"}></div>
+      <div className={"bg-blue-300 w-full h-10"}></div>
+      <div className={"bg-blue-400 w-full h-10"}></div>
+      <div className={"bg-blue-500 w-full h-10"}></div>
+      <div className={"bg-blue-600 w-full h-10"}></div>
+    </div>
+  )
+}
