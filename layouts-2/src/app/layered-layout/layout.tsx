546bc36cb64b3dcf4bb7bfabd6c9e990576a0565
theodor349 <theodor349@gmail.com>
2025-06-07 19:08:10 +0200

    Calendar Layered Layout done

---
+++ layouts-2/src/app/layered-layout/layout.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { SquareArrowLeft, SquareArrowRight, Menu } from 'lucide-react';
+
+export default function LearningLayout({children,}: {children: React.ReactNode}) {
+  return (
+    <div className="flex flex-col h-screen bg-[#e9e2f3]">
+      {/* flex-grow, enables the element to grow on the vertical axis to take up all remaining space within its parent */}
+      {/* w-full, sets its width to the parents' */}
+      {/* overflow-auto, enables the content within to be scrolled if it is to large */}
+      <div className={"flex-grow w-full overflow-auto px-4"}>
+        { children}
+      </div>
+
+
+      <div className={"bg-[#b090e0] flex flex-row justify-between items-end w-full p-2 rounded-t-2xl"}>
+
+        {/* Week and day of week */}
+        <div className={"bg-[#400e8b] text-white text-lg w-12 h-12 rounded-2xl flex flex-col items-center justify-center"}>
+          4
+        </div>
+
+        {/* Date and controls */}
+        <div className={"flex flex-col items-center text-white"}>
+          <div className={"text-sm font-thin"}>
+            Thursday
+          </div>
+          <div className={"flex flex-col items-center flex-grow"}>
+            <div className={"flex flex-row items-center gap-2 h-12 bg-[#400e8b] text-white text-lg rounded-2xl"}>
+              <button className={"m-2 font-bold"}>
+                <SquareArrowLeft />
+              </button>
+              <div className={"text-sm font-semibold"}>
+                Jan 23, 2025
+              </div>
+              <button className={"m-2 font-bold"}>
+                <SquareArrowRight />
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/* Options */}
+        <div className={"flex flex-row"}>
+          <div className={"bg-[#400e8b] text-white text-lg w-12 h-12 rounded-2xl flex flex-col items-center justify-center"}>
+            <Menu/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- layouts-2/src/app/layered-layout/page.tsx
+++ layouts-2/src/app/layered-layout/page.tsx
@@ -1,42 +1,42 @@
 export default function Page() {
   return(
-    <div className={"flex items-center flex-col md:flex-row flex-wrap p-4"}>
-      <div className={"bg-red-100 w-full h-10"}></div>
-      <div className={"bg-red-200 w-full h-10"}></div>
-      <div className={"bg-red-300 w-full h-10"}></div>
-      <div className={"bg-red-400 w-full h-10"}></div>
-      <div className={"bg-red-500 w-full h-10"}></div>
-      <div className={"bg-red-600 w-full h-10"}></div>
-      <div className={"bg-green-100 w-full h-10"}></div>
-      <div className={"bg-green-200 w-full h-10"}></div>
-      <div className={"bg-green-300 w-full h-10"}></div>
-      <div className={"bg-green-400 w-full h-10"}></div>
-      <div className={"bg-green-500 w-full h-10"}></div>
-      <div className={"bg-green-600 w-full h-10"}></div>
-      <div className={"bg-blue-100 w-full h-10"}></div>
-      <div className={"bg-blue-200 w-full h-10"}></div>
-      <div className={"bg-blue-300 w-full h-10"}></div>
-      <div className={"bg-blue-400 w-full h-10"}></div>
-      <div className={"bg-blue-500 w-full h-10"}></div>
-      <div className={"bg-blue-600 w-full h-10"}></div>
-      <div className={"bg-red-100 w-full h-10"}></div>
-      <div className={"bg-red-200 w-full h-10"}></div>
-      <div className={"bg-red-300 w-full h-10"}></div>
-      <div className={"bg-red-400 w-full h-10"}></div>
-      <div className={"bg-red-500 w-full h-10"}></div>
-      <div className={"bg-red-600 w-full h-10"}></div>
-      <div className={"bg-green-100 w-full h-10"}></div>
-      <div className={"bg-green-200 w-full h-10"}></div>
-      <div className={"bg-green-300 w-full h-10"}></div>
-      <div className={"bg-green-400 w-full h-10"}></div>
-      <div className={"bg-green-500 w-full h-10"}></div>
-      <div className={"bg-green-600 w-full h-10"}></div>
-      <div className={"bg-blue-100 w-full h-10"}></div>
-      <div className={"bg-blue-200 w-full h-10"}></div>
-      <div className={"bg-blue-300 w-full h-10"}></div>
-      <div className={"bg-blue-400 w-full h-10"}></div>
-      <div className={"bg-blue-500 w-full h-10"}></div>
-      <div className={"bg-blue-600 w-full h-10"}></div>
+    <div className={"w-full"}>
+
+      <HourDisplay/>
+
     </div>
+  )
+}
+
+function HourDisplay() {
+  const hours = Array.from({ length: 24 }, (_, i) => i + 1);
+
+  return (
+    <div className="w-full grid grid-cols-[30px_1fr] items-end gap-x-2"> {/* Example grid layout */}
+      {hours.map((hour) => (
+        <HourLine key={hour} hour={hour} />
+      ))}
+    </div>
+  );
+}
+
+
+interface HourLineProps {
+  hour: number;
+}
+
+function HourLine({hour}: HourLineProps) {
+  return (
+    <>
+      <div className={"text-sm text-right text-[#400e8b]"}>
+        {hour === 24 ? "" : `${hour.toString().padStart(2, '0')}:00`}
+      </div>
+      <div className={"flex flex-col gap-10"}>
+        <div/>
+        <div className={"w-[calc(100%+38px)] ml-[-38px] border-b-2 border-[#b090e0] border-dotted"}/>
+        <div className={"w-[calc(100%+38px)] ml-[-38px] border-b-2 border-[#b090e0] border-dotted"}/>
+        {hour === 24 ? <div/> : <div className={"w-[calc(100%+38px)] border-b-2 border-[#b090e0] ml-[-38px]"} />}
+      </div>
+    </>
   )
 }
