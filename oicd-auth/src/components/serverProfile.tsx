eefaddf2bf6e6b34db700e541938a75f3f02d0e6
theodor349 <theodor349@gmail.com>
2025-06-11 10:54:03 +0200

    AUth0 Implemented

---
+++ oicd-auth/src/components/serverProfile.tsx
@@ -0,0 +1,7 @@
+import { auth0 } from "@/lib/auth0";
+
+export default async function ServerProfile() {
+  const session = await auth0.getSession();
+  const user = session?.user;
+  return ( user && ( <div> <img src={user.picture} alt={user.name}/> <h2>{user.name}</h2> <p>{user.email}</p> </div> )  );
+}
