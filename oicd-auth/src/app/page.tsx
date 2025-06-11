9c7d4a83746a651f0d7c54d453ed09ccb9bd56d1
theodor349 <theodor349@gmail.com>
2025-06-11 11:04:24 +0200

    Added Auth and no Auth pages. Need to setup auth logic for protecting pages

--- oicd-auth/src/app/page.tsx
+++ oicd-auth/src/app/page.tsx
@@ -2,13 +2,11 @@ import ClientProfile from '@/components/clientProfile'
 import ServerProfile from '@/components/serverProfile'
 import {auth0} from "@/lib/auth0";
 
-export default function Home() {
+export default async function Home() {
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <a href="/auth/login">Login</a>
-        <a href="/auth/logout">Logout</a>
-
         <div>
           <ClientProfile />
         </div>
