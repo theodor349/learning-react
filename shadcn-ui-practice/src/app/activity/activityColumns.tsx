afe8d5afd5addbc75773006c0db5ca39cde86254
theodor349 <theodor349@gmail.com>
2025-06-16 19:23:00 +0200

    Options Button

--- shadcn-ui-practice/src/app/activity/activityColumns.tsx
+++ shadcn-ui-practice/src/app/activity/activityColumns.tsx
@@ -1,6 +1,18 @@
 "use client"
 
 import { ColumnDef } from "@tanstack/react-table"
+import { MoreVertical } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -28,4 +40,23 @@ export const columns: ColumnDef<Activity>[] = [
     accessorKey: "category.name",
     header: "Category",
   },
+  {
+    id: "actions",
+    cell: ({row}) => {
+      const payment = row.original
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <MoreVertical className="h-4 w-4"/>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem>Change Category</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    }
+  }
 ]
