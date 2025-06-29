4fed3615a46aa3faaa62012f44f9dbf1fd5c8a04
theodor349 <theodor349@gmail.com>
2025-06-29 17:39:31 +0200

    Remove Provider

--- global-state-spacetimedb/src/app/layout.tsx
+++ global-state-spacetimedb/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import SpacetimeDBProvider from "@/providers/spacetimedb-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SpacetimeDBProvider>
           {children}
-        </SpacetimeDBProvider>
       </body>
     </html>
   );
